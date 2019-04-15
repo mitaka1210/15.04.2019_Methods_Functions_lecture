efd31e5e18ca6ce243dd383f8385a81481750423
Dimitar <mitaka1210@github.com>
2019-04-15 11:38:33 +0300

    Add project files.

---
+++ 15.04.2019_Methods_Functions_lecture/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _15._04._2019_Methods_Functions_lecture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
